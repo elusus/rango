48bafb48ea17c3d82690e18b93e65cb3f9f13f4b
vagrant <vagrant@precise64.(none)>
2013-10-24 20:32:11 +0000

    Finished part 3 of the tutorial

--- rango/views.py
+++ rango/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse(
+		"Rango says hello world!<br><br><a href='about/'>Here is a link to the about page</a>"
+		)
+
+def about_page(request):
+	return HttpResponse(
+		"Rango says: Here is the about page.<br><br><a href='/rango/'>Here is a link back to the about page</a>"
+		)
